60b7c63a2159be694e677117b4f08c2d90837a0f
admin <admin@PC04>
2022-11-01 17:55:28 +0900

    ddddd 롬복까지

---
+++ src/ch21/TestDTO.java
@@ -0,0 +1,23 @@
+package ch21;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter // 
+@ToString // 투스트링 
+@AllArgsConstructor // 모든생성자 
+@NoArgsConstructor // 기본생성자 
+public class TestDTO {
+	
+	private String s1;
+	private String s2;
+	private String s3;
+	
+	
+	
+
+}
---
+++ src/ch21/Testmain.java
@@ -0,0 +1,22 @@
+package ch21;
+
+public class Testmain {
+
+	public static void main(String[] args) {
+		
+		TestDTO t1 = new TestDTO();
+		
+		System.out.println(t1.get);
+
+		TestDTO2 t2 = new TestDTO2();
+		
+		System.out.println(t2.getP1());
+		System.out.println(t2.getP2());
+		System.out.println(t2.getP3());
+		
+		
+		
+
+	}
+
+}
